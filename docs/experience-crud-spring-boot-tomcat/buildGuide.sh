83369b5ba3f555a018052c9bbcc8d7328897fdae
Stefan Sitani <ssitani@redhat.com>
2017-03-24 19:05:33 +0100

    docs for the CRUD (JDBC) experiences
    output guide names in build scripts set to correct values
    
    WIP: added guide modules, cleanup required
    
    name scrub, CRUD  modular structure
    
    vertx crud master.adoc now in correct dir
    
    reference removal
    
    rewrites to the introduction topic module
    
    removet specific URLs from the database intraction module
    
    further edits to accommodate latest changes in README files
    
    renamed files to match naming pattern
    
    changed symlink path to topics folderto relative
    
    section title fix
    
    markup fixes based on peer review
    
    title case fix
    
    scrapped pipeline section, added url variables for openshift online
    
    replaced title variable
    
    fixed heading in master.adoc
    
    refactored guide name variables
    
    fixed topic includes files not yet populated
    
    crud guides common topics
    
    common intro section for JDBC
    
    interaction section updated
    
    added database deployment an d pipeline build for vertx
    
    replaced quickstart with booster
    
    fixed conflicts in attributes file
    
    another fix to document-attributes.adoc
    
    replaced <appplication_URL> placeholders wiht variables
    
    linked deploymet instructions from http api
    
    minor ID fix
    
    resolved duplicit IDs, removed coflicting section from attribute file

---
+++ docs/experience-crud-spring-boot-tomcat/buildGuide.sh
@@ -0,0 +1,5 @@
+
+#Name of the html output file
+GUIDE_HTML_NAME=experience-crud-spring-boot-tomcat.html
+
+asciidoctor master.adoc -o ../../html/$GUIDE_HTML_NAME
---
+++ docs/experience-crud-vertx/buildGuide.sh
@@ -0,0 +1,5 @@
+
+#Name of the html output file
+GUIDE_HTML_NAME=experience-crud-vertx.html
+
+asciidoctor master.adoc -o ../../html/$GUIDE_HTML_NAME
