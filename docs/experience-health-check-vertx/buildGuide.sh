d818e997c712935c3bfe42c82a27494befa29799
Zach Rhoads <zach@zachrhoads.com>
2017-03-28 14:49:59 -0500

    PR #30: Added health check experience

---
+++ docs/experience-health-check-vertx/buildGuide.sh
@@ -0,0 +1,9 @@
+
+#Name of the html output file
+GUIDE_HTML_NAME=experience-health-check-vertx.html
+OUTPUT_DIR=../../html
+
+rm -r $OUTPUT_DIR/images/
+cp -r topics/images/ $OUTPUT_DIR/images/
+
+asciidoctor master.adoc -o $OUTPUT_DIR/$GUIDE_HTML_NAME
