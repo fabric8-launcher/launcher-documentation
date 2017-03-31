0c369251b6f0fb0ccb7de47100dfea184c5b56fc
Zach Rhoads <zach@zachrhoads.com>
2017-03-31 10:17:32 -0500

    added first pass at configmap experience

--- docs/experience-configmap-spring-boot-tomcat/buildGuide.sh
+++ docs/experience-configmap-spring-boot-tomcat/buildGuide.sh
@@ -1,6 +1,6 @@
 
 #Name of the html output file
-GUIDE_HTML_NAME=experience-crud-wf-swarm.html
+GUIDE_HTML_NAME=experience-configmap-spring-boot-tomcat.html
 OUTPUT_DIR=../../html
 
 rm -r $OUTPUT_DIR/images/
