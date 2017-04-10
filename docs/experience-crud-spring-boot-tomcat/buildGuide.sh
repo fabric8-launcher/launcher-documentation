2fe91b46b6b6cd6e740d314e4f9bc313306b5d96
Zach Rhoads <zach@redhat.com>
2017-04-10 16:46:23 -0500

    refactored guides for issue #40 and #59

--- docs/experience-crud-spring-boot-tomcat/buildGuide.sh
+++
@@ -1,9 +0,0 @@
-
-#Name of the html output file
-GUIDE_HTML_NAME=experience-crud-spring-boot-tomcat.html
-OUTPUT_DIR=../../html
-
-rm -r $OUTPUT_DIR/images/
-cp -r topics/images/ $OUTPUT_DIR/images/
-
-asciidoctor master.adoc -o $OUTPUT_DIR/$GUIDE_HTML_NAME
---
+++ docs/mission-http-api-spring-boot-tomcat/buildGuide.sh
@@ -0,0 +1,9 @@
+
+#Name of the html output file
+GUIDE_HTML_NAME=mission-http-api-spring-boot-tomcat.html
+OUTPUT_DIR=../../html
+
+rm -r $OUTPUT_DIR/images/
+cp -r topics/images/ $OUTPUT_DIR/images/
+
+asciidoctor master.adoc -o $OUTPUT_DIR/$GUIDE_HTML_NAME
