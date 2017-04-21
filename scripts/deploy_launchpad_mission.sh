723a61f4e84b9a35ef1816765ce48a8d1829bc89
Charles Moulliard <ch007m@gmail.com>
2017-04-21 17:27:56 +0200

    Remove LAUNCHPAD_BACKEND_CATALOG_ env vars (#149)

--- scripts/deploy_launchpad_mission.sh
+++ scripts/deploy_launchpad_mission.sh
@@ -74,9 +74,6 @@ oc new-app launchpad -n $PROJECTNAME \
     -p LAUNCHPAD_KEYCLOAK_REALM= \
     -p LAUNCHPAD_MISSIONCONTROL_OPENSHIFT_USERNAME=${IDENTITY[0]} \
     -p LAUNCHPAD_MISSIONCONTROL_OPENSHIFT_PASSWORD=${IDENTITY[1]} \
-    -p LAUNCHPAD_BACKEND_CATALOG_GIT_REPOSITORY=https://github.com/openshiftio/booster-catalog.git \
-    -p LAUNCHPAD_BACKEND_CATALOG_GIT_REF=master \
-    -p LAUNCHPAD_BACKEND_CATALOG_INDEX_PERIOD="0" \
     -p LAUNCHPAD_FRONTEND_HOST=launchpad-frontend-$PROJECTNAME.$HOSTNAMEORIP.nip.io
 echo "------------------------------------------"
 
