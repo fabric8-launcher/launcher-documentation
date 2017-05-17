5b2e81ab18542c46ffd216f41e74556bec022a3e
Zach Rhoads <zach@redhat.com>
2017-05-17 14:14:27 -0500

    Issue #239: Add scaffold for contrib guide

---
+++ docs/contrib-guide/buildGuide.sh
@@ -0,0 +1,7 @@
+GUIDE_HTML_NAME=contrib-guide.html
+OUTPUT_DIR=../../html
+
+rm -r $OUTPUT_DIR/images/
+cp -r topics/images/ $OUTPUT_DIR/images/
+
+asciidoctor master.adoc -o $OUTPUT_DIR/$GUIDE_HTML_NAME
