0ee6c89e3242df22399fe5bc4089b1e4a73ff0ac
Zach Rhoads <zach@redhat.com>
2017-05-24 10:50:28 -0500

    Refactor: added dev guides removed mission guides

--- docs/mission-http-api-vertx/buildGuide.sh
+++
@@ -1,9 +0,0 @@
-
-#Name of the html output file
-GUIDE_HTML_NAME=mission-http-api-vertx.html
-OUTPUT_DIR=../../html
-
-rm -r $OUTPUT_DIR/images/
-cp -r topics/images/ $OUTPUT_DIR/images/
-
-asciidoctor master.adoc -o $OUTPUT_DIR/$GUIDE_HTML_NAME
