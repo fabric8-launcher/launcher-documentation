3febad4148e9c071543168a38008b499f4b6c2c9
Zach Rhoads <zach@redhat.com>
2017-06-13 09:29:45 -0500

    Update: rework getting started guide, related to #243

--- scripts/previewDocsServer.sh
+++ scripts/previewDocsServer.sh
@@ -4,4 +4,4 @@ docker build --force-rm -t docs:latest scripts/.
 HOST_GIT_REPO_DIR="/path/to/documentation"
 IMG_GIT_REPO_DIR="/documentation"
 
-docker run -ti -p 8080:8080 -v $HOST_GIT_REPO_DIR:$IMG_GIT_REPO_DIR docs:latest /bin/bash -c 'cd '$IMG_GIT_REPO_DIR'; rm -r ci/openshiftio-appdev-docs/src/main/resources/webroot/docs/*; ./scripts/buildGuides.sh; mkdir -p ci/openshiftio-appdev-docs/src/main/resources/webroot; cp -r html/ ci/openshiftio-appdev-docs/src/main/resources/webroot/docs; cd ci/openshiftio-appdev-docs; mvn clean compile vertx:run;'
+docker run -ti -p 8080:8080 -v $HOST_GIT_REPO_DIR:$IMG_GIT_REPO_DIR docs:latest /bin/bash -c 'cd '$IMG_GIT_REPO_DIR'; rm -r ci/openshiftio-appdev-docs/src/main/resources/webroot/docs/*; ./scripts/buildGuides.sh; mkdir -p ci/openshiftio-appdev-docs/src/main/resources/webroot; cp -r html/* ci/openshiftio-appdev-docs/src/main/resources/webroot/docs/; cd ci/openshiftio-appdev-docs; mvn clean compile vertx:run;'
