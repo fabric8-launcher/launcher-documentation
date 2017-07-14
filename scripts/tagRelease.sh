e2057d8d6eea8296d70dcf17b15eaf01e81ea5cc
Tomas Radej <tradej@redhat.com>
2017-07-14 16:59:39 +0200

    Tagging script and instructions
    * RELEASE.adoc update with information about how to tag
    * Added the tagRelease.sh script
    * Jenkins includes RFC 3339 date instead of the commit hash

---
+++ scripts/tagRelease.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Not tagging if HEAD is already tagged
+if git name-rev --tags --name-only --no-undefined HEAD &>/dev/null; then
+	read -p "Your latest commit is tagged. Do you really want to re-tag? [yN] " choice
+	case $choice in
+		[Yy] ) :;;
+		* ) exit;;
+	esac
+fi
+
+# Parsing current tag
+
+# Using name-rev is not good because it only gives you one tag, and not
+# necessary the latest version
+today_tag="$(date --rfc-3339=date)"
+latest_today_tag="$(git tag | grep "$today_tag" | sort | tail -1)"
+suffix="$(echo $latest_today_tag | awk -F '_' '{print $2}')"
+
+# We released exactly once today
+if [ "$latest_today_tag" == "$today_tag" ]; then
+	tag="${today_tag}_2"
+# We released today at least twice
+elif [ "$suffix" != "" ]; then
+	tag="${today_tag}_$((suffix+1))"
+# We have not released today yet
+else
+	tag="${today_tag}"
+fi
+
+echo Tagging with "${tag}".
+echo Do not forget to push the tags: git push --tags \$REMOTE
+
+git tag "${tag}"
