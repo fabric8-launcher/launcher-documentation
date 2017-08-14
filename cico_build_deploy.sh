2d72b9dfbd0d55266a6cb769d34aa7ad20fbe4e2
Václav Pavlín <vaclav.pavlin@gmail.com>
2017-08-14 12:22:59 +0200

    Use DEVSHIFT_TAG_LEN to generate tag

--- cico_build_deploy.sh
+++ cico_build_deploy.sh
@@ -9,7 +9,6 @@ DOCKER_HUB_URL="openshiftio/appdev-documentation"
 BUILDER_IMAGE="appdev-documentation-builder"
 BUILDER_CONT="appdev-documentation-builder-container"
 DEPLOY_IMAGE="appdev-documentation-deploy"
-TAG_LENGTH=7
 
 TARGET_DIR="html"
 
@@ -65,7 +64,7 @@ docker build -t ${DEPLOY_IMAGE} -f Dockerfile.deploy .
 
 #PUSH
 if [ -z $CICO_LOCAL ]; then
-    TAG=$(echo $GIT_COMMIT | cut -c1-${TAG_LENGTH})
+    TAG=$(echo $GIT_COMMIT | cut -c1-${DEVSHIFT_TAG_LEN})
     tag_push "${REGISTRY_URL}:${TAG}" ${DEVSHIFT_USERNAME} ${DEVSHIFT_PASSWORD} ${REGISTRY_URI}
     tag_push "${REGISTRY_URL}:latest" ${DEVSHIFT_USERNAME} ${DEVSHIFT_PASSWORD} ${REGISTRY_URI}
 
