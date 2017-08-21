0d7951a7c127e25f75ef7516b581401ff7498e91
Zach Rhoads <zach@redhat.com>
2017-08-21 14:45:37 -0500

    Cleanup: added dir permission setting to script and .gitignore
    Fixes #515 #517

--- cico_build_deploy.sh
+++ cico_build_deploy.sh
@@ -52,9 +52,16 @@ rm -rf ${TARGET_DIR}/
 docker build -t ${BUILDER_IMAGE} -f Dockerfile.build .
 
 mkdir ${TARGET_DIR}/
+mkdir ${TARGET_DIR}/images
+
 docker run --detach=true --name ${BUILDER_CONT} -t -v $(pwd)/${TARGET_DIR}:/${TARGET_DIR}:Z ${BUILDER_IMAGE} /bin/tail -f /dev/null #FIXME
 
+
 docker exec ${BUILDER_CONT} sh scripts/build_guides.sh
+
+#Need to do this again to set permission of images and html files
+chmod -R 0777 ${TARGET_DIR}/
+
 
 #BUILD DEPLOY IMAGE
 docker build -t ${DEPLOY_IMAGE} -f Dockerfile.deploy .
