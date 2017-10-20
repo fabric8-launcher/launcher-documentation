b68f493f178c89faff36b9a240cc98f11fe7af40
Tomas Radej <tradej@redhat.com>
2017-10-20 14:52:22 +0200

    Reintroduced preview server script for container deployment

---
+++ scripts/preview_server.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+SCRIPT_SRC="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P )"
+
+pushd $SCRIPT_SRC/.. &>/dev/null
+if [[ $(id -u) -ne 0 ]]; then
+    sudo CICO_LOCAL=true $SCRIPT_SRC/../cico_build_deploy.sh && sudo docker run -p 80:8080 -it appdev-documentation-deploy:latest
+else
+    CICO_LOCAL=true $SCRIPT_SRC/../cico_build_deploy.sh && docker run -p 80:8080 -it appdev-documentation-deploy:latest
+fi
+popd &>/dev/null
+
