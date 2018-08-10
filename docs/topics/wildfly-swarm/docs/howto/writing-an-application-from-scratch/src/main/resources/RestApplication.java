ab77e04ebacd1135c180cd2f2bd152ee44099af8
Tomas Radej <tradej@redhat.com>
2018-08-10 15:30:09 +0200

    Removed the WFS synced directory

--- docs/topics/wildfly-swarm/docs/howto/writing-an-application-from-scratch/src/main/resources/RestApplication.java
+++
@@ -1,8 +0,0 @@
-package com.example.rest;
-
-import javax.ws.rs.core.Application;
-import javax.ws.rs.ApplicationPath;
-
-@ApplicationPath("/rest")
-public class RestApplication extends Application {
-}
