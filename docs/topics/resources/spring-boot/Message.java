77c644340c062ad67bdccb7abaf90c3c4eb35029
Tomas Radej <tradej@redhat.com>
2019-03-06 12:02:02 +0100

    RHOARDOC-1539: Updated app example for SB 2.1

---
+++ docs/topics/resources/spring-boot/Message.java
@@ -0,0 +1,15 @@
+package com.example;
+
+public class Message {
+
+    private String content = "Greetings!";
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+}
