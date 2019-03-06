1635f47b791470e1d1ec98fb736385efe5958228
Tomas Radej <tradej@redhat.com>
2019-03-06 12:02:02 +0100

    RHOARDOC-1539: Updated app example for SB 2.1

---
+++ docs/topics/resources/spring-boot/Controller.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/")
+    public Message displayMessage() {
+        return new Message();
+    }
+
+}
